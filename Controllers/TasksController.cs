3b6cf479d2e24d60ed864598a2dc8bbcbc7f9370
Ravindu007 <ravindu0504@gmail.com>
2023-10-02 23:55:14 +0530

    Add project files.

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskAPIProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        //get all tasks
+        [HttpGet]
+        public IActionResult Tasks()
+        {
+            var tasks = new string[] { "Task1", "Task2", "Task3" }; 
+            return Ok(tasks);
+        }
+    }
+}
